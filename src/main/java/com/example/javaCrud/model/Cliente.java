2421f2977eea2de9d7372bd1ad34f4672d4091bc
Caroline Barbara <caroline.sbarbara@gmail.com>
2025-03-29 18:52:27 -0300

    inserção de dados na classe Cliente

--- src/main/java/com/example/javaCrud/model/Cliente.java
+++ src/main/java/com/example/javaCrud/model/Cliente.java
@@ -1,5 +1,29 @@
 package com.example.javaCrud.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "clientes")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Cliente {
 
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    private String nome;
+    private int idade;
+    private String sexo;
+    private String cidade;
+    
 }
