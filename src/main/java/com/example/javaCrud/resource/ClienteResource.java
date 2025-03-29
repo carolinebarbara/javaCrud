26c0291f7c9859a2e2157fdcc407c8d41a62d850
Caroline Barbara <caroline.sbarbara@gmail.com>
2025-03-29 19:30:42 -0300

    atualizacao do resource

--- src/main/java/com/example/javaCrud/resource/ClienteResource.java
+++ src/main/java/com/example/javaCrud/resource/ClienteResource.java
@@ -1,16 +1,61 @@
 package com.example.javaCrud.resource;
 
+import java.util.List;
+
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.javaCrud.model.Cliente;
+import com.example.javaCrud.service.ClienteService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+
+
 @RestController
-@RequestMapping("cliente")
+@RequestMapping("/clientes")
 public class ClienteResource {
-    //@Operation(summary = "Find all countries with pagination")
-    @GetMapping
-    public ResponseEntity<String> list() {
-        return ResponseEntity.ok("Olá, pessoa!");
+
+    private final ClienteService service;
+
+    public ClienteResource(ClienteService service){
+        this.service = service;
     }
+
+    @GetMapping
+    public ResponseEntity<List<Cliente>> listarTodos() {
+        return ResponseEntity.ok(service.listarTodos());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Cliente> buscarPorId(@PathVariable Long id) {
+        Cliente cliente = service.buscarPorId(id);
+        return cliente != null ? ResponseEntity.ok(cliente) : ResponseEntity.notFound().build();
+    }
+    
+    @PostMapping
+    public ResponseEntity<Cliente> salvar(@RequestBody Cliente cliente){
+        return ResponseEntity.ok(service.salvar(cliente));
+    }
+    
+    @PutMapping("/{id}")
+    public ResponseEntity<Cliente> atualizar(@PathVariable Long id, @RequestBody Cliente cliente){
+        Cliente atualizado = service.atualizar(id, cliente);
+        return atualizado != null ? ResponseEntity.ok(atualizado) : ResponseEntity.notFound().build();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deletar(@PathVariable Long id){
+        service.deletar(id);
+        return ResponseEntity.noContent().build();
+    }
+    
+    
+
+
 }
